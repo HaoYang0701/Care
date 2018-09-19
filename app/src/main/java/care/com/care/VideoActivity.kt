ad3d0c24cc9c6873cd5ced288d2e02aef9859e2b
HaoYang0701 <yang.hao0701@gmail.com>
2018-09-18 21:10:12 -0400

    Added apk

--- app/src/main/java/care/com/care/VideoActivity.kt
+++ app/src/main/java/care/com/care/VideoActivity.kt
@@ -33,11 +33,6 @@ import kotlinx.android.synthetic.main.content_video.*
 
 class VideoActivity : AppCompatActivity() {
     private val CAMERA_MIC_PERMISSION_REQUEST_CODE = 1
-    private val TAG = "VideoActivity"
-    private val API_KEY = "SK86d31ae222282433f99ca24725e0f4c5"
-    private val API_SECRET = "OAZ3wsX2iPHVtTlP6m7CQEGywvrxxR3N"
-    private val ACCOUNT_ID = "ACb67d589e0ce9d697339d8ca0092b0b05"
-    private var username : String = ""
 
     private val apiService by lazy {
         ApiService.createRetrofitService()
